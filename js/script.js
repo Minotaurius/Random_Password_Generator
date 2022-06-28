81bf65e5ce0f2a039ac8a5c2abf2a428d6a48faa
Minotaurius <brandonslanger@gmail.com>
2022-06-27 22:17:05 -0500

    Saving progress on pw generator

--- js/script.js
+++ js/script.js
@@ -1,27 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Setting our prompts as strings to be referenced later
-const specialCharacters = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~' ;
-// Testing to make sure all special characters appear in selector
-console.log(specialCharacters);
-const numbers = '1234567890';
-const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//Setting our variables as strings to be referenced randomly later for password generation
+
 const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers = '1234567890';
+const specialCharacters = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~' ;
+
+
+//Setting placeholders to store user values entered below
+const passOptions = ''
+const passHolder = ''
 
 function generatePassword() {
   var passLength = prompt('Please choose a password length for your random generated password between 8 and 128 characters');
     if(passLength < 8) {
-    alert('Oops! That is too small. Please pick a number at LEAST 8 or higher');
-  }
-  if(passLength > 128) {
-    alert('Oops! That number is too large. Please pick a smaller number that is 128 characters OR less')
-  }
-  var passLowerCase = confirm('Press "OK" you like your random generated password to include lowercase letters?')
-  var passUpperCase = confirm('Press "OK" if you like your random generated password to include uppercase letters?')
-  var passNumerics = confirm('Press "OK" if you would like your random generated password to include numbers?')
-  var passSpecialChars = confirm('Press "OK" if you would like your random generated password to include special characters?')
+      alert('Oops! Please make sure to select a number between 8 and 128 characters :)');
+      var passLength = prompt('Please choose a password length for your random generated password between 8 and 128 characters');
+    } else if(passLength > 128) {
+      alert('Oops! Please make sure to select a number between 8 and 128 characters :)');
+      var passLength = prompt('Please choose a password length for your random generated password between 8 and 128 characters');
+    }
 
+  var passLowerCase = confirm('Press "OK" you like your random generated password to include lowercase letters?');
+  var passUpperCase = confirm('Press "OK" if you like your random generated password to include uppercase letters?');
+  var passNumerics = confirm('Press "OK" if you would like your random generated password to include numbers?');
+  var passSpecialChars = confirm('Press "OK" if you would like your random generated password to include special characters?');
+
+    if(passLowerCase) {
+      passOptions = passOptions += lowercase;
+    }
+    if(passUpperCase) {
+      passOptions = passOptions += uppercase;
+    }
+    if(passNumerics) {
+      passOptions = passOptions += numbers;
+    }
+    if(passSpecialChars) {
+      passOptions = passOptions += specialCharacters;
+    }
 
 }
 
